859f1c1bfeba19038541ba489e3dfad8c374fa81
Cédric Donner <cedonner@gmail.com>
2015-03-11 11:22:02 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name = "csudoci",
+    packages = ["csudoci"],
+    version = "0.1",
+    description = "Libraries for CSUD OCI",
+    author = "Cédric Donner",
+    author_email = "cedonner@gmail.com",
+    url = "https://github.com/oci1315/csudoci",
+    download_url = "https://github.com/oci1315/csudoci/releases",
+    keywords = ["teaching", "computer science", "Switzerland", "Collège du Sud"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: Development",
+        "Environment :: Other Environment",
+        "Intended Audience :: Teacher, students",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Teaching :: Libraries :: Python Modules",
+        ],
+    long_description = """\
+This package contains modules and libraries nended for the computer science course
+"Option complémentaire informatique" at College du Sud, 1630 Bulle, Switzerland
+"""
+)
