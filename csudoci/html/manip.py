bfdf338a392ce3032db015f546dacb5bf3ad571d
Cédric Donner <cedonner@gmail.com>
2015-03-31 14:24:06 +0200

    correction extraction de liens et constructions de liste ul > li > a

--- csudoci/html/manip.py
+++ csudoci/html/manip.py
@@ -8,7 +8,7 @@ généré par ``csudoci.html.parser``.
 
 '''
 
-from csudoci.html.htmltree import Ul, Li
+from csudoci.html.htmltree import Ul, Li, ElementList
 from csudoci.html.parser import HTMLTreeParser
 
 
@@ -25,13 +25,21 @@ def get_links(tree):
     return links
 
 
-def get_html_links(tree):
+def get_links_as_ul(tree):
     ul_links = Ul()
 
     for link in get_links(tree):
         link < Li() < ul_links
 
     return ul_links
+
+
+def get_links_as_ul2(tree):
+    link_ul = ElementList(
+        [ Li().add_child(link) for link in get_links(tree) ]
+    ) < Ul()
+
+    return link_ul
 
 
 def test():
@@ -57,10 +65,13 @@ def test():
     p.feed(test_html)
     tree = p.get_tree()
 
-    list_of_links = get_html_links(tree)
+    list_of_links = get_links_as_ul(tree)
     html = list_of_links.html()
     print(html)
 
+    list_of_links2 = get_links_as_ul2(tree)
+    html = list_of_links2.html()
+    print(html)
+
 if __name__ == '__main__':
-    
     test()
